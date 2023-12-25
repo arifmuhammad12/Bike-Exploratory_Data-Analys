e3acbc2935f8181d8b2dd2d8c50f4c55add85b4b
arifmuhammad12 <97782257+arifmuhammad12@users.noreply.github.com>
2023-12-25 18:31:07 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-bike_df = pd.read_csv("https://raw.githubusercontent.com/arifmuhammad12/Bike-Exploratory_Data-Analys/blob/main/dashboard/bike_data.csv")
+bike_df = pd.read_csv("https://raw.githubusercontent.com/arifmuhammad12/Bike-Exploratory_Data-Analys/main/dashboard/bike_data.csv")
 
 datetime_columns = ["date"]
 
